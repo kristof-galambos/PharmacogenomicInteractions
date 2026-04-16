5aab08467f264c96c6599eb202cf0a88f951a5d6
kristof <kris.galambos@gmail.com>
2026-04-16 16:10:49 +0200

    Bootstrap dataset

--- src/data_split.py
+++ src/data_split.py
@@ -1,19 +1,19 @@
-def split_data(X, y):
-    shuffled_idx = X.sample(frac=1, random_state=42).index
+def split_data(X, y, random_state=42):
+    bootstrap_idx = X.sample(n=len(X), replace=True, random_state=random_state).index
 
-    X_shuffled = X.loc[shuffled_idx].reset_index(drop=True)
-    y_shuffled = y.loc[shuffled_idx].reset_index(drop=True)
+    X_bootstrap = X.loc[bootstrap_idx].reset_index(drop=True)
+    y_bootstrap = y.loc[bootstrap_idx].reset_index(drop=True)
 
-    n = len(X_shuffled)
+    n = len(X_bootstrap)
     train_end = int(0.8 * n)
     val_end = int(0.9 * n)
 
-    X_train = X_shuffled.iloc[:train_end]
-    X_val = X_shuffled.iloc[train_end:val_end]
-    X_test = X_shuffled.iloc[val_end:]
+    X_train = X_bootstrap.iloc[:train_end]
+    X_val = X_bootstrap.iloc[train_end:val_end]
+    X_test = X_bootstrap.iloc[val_end:]
 
-    y_train = y_shuffled.iloc[:train_end]
-    y_val = y_shuffled.iloc[train_end:val_end]
-    y_test = y_shuffled.iloc[val_end:]
+    y_train = y_bootstrap.iloc[:train_end]
+    y_val = y_bootstrap.iloc[train_end:val_end]
+    y_test = y_bootstrap.iloc[val_end:]
 
     return X_train, X_val, X_test, y_train, y_val, y_test
